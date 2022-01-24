cc6c5df4b36e4c96a6f0c76e908acc204a438221
dhimaseka <dimaseka83@gmail.com>
2022-01-24 15:32:13 +0700

    push

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-function minMax(arrayOfNumbers) {
-    let currentMin = arrayOfNumbers[0];
-    let currentMax = arrayOfNumbers[0];
-    for (value of arrayOfNumbers) {
-        if (value < currentMin) {
-            currentMin = value;
-        } else if (value > currentMax) {
-            currentMax = value;
-        }
+const mail = {
+    from : "pengirim@dicoding.com",
+    sendMessage: function (msg, to) {
+        console.log(`you send: ${msg} to ${to} from ${this.from}`);
     }
-
-    console.log(`currentMin: ${currentMin}, currentMax: ${currentMax}`);
 }
 
-minMax([8, -6, 0, 9, 40, 2, 23, 50, 2, -3, -15, 15, -23, 71]);
+console.log(mail.from);
+mail.sendMessage('apa kabar', 'penerima@dicoding.com')
