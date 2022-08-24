642baa737988e80dcd7af55b40530dc865a06524
dhimaseka <dimaseka83@gmail.com>
2022-08-24 12:56:33 +0700

    ++

--- dicoding-frontend/movie-catalogue/src/scripts/routes/routes.js
+++ dicoding-frontend/movie-catalogue/src/scripts/routes/routes.js
@@ -1,12 +1,14 @@
 import NowPlaying from '../views/page/now-playing';
 import Upcoming from '../views/page/upcoming';
 import Detail from '../views/page/detail';
+import Like from '../views/page/like';
 
 const routes = {
   '/': NowPlaying,
   '/now-playing': NowPlaying,
   '/upcoming': Upcoming,
   '/detail/:id': Detail,
+  '/like': Like,
 };
 
 export default routes;
