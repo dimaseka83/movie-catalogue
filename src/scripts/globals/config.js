398ad3b13c8cbe10b57fabb6242a70d8e3d13aec
dhimaseka <dimaseka83@gmail.com>
2022-09-06 14:51:50 +0700

    ++

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'a62f7828d46c5b01c3fcd4694e6ee9ff',
+  KEY: process.env.API_KEY_TMDB,
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
--- webpack.common.js
+++ webpack.common.js
@@ -3,47 +3,63 @@ const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  entry: {
-    app: path.resolve(__dirname, 'src/scripts/index.js'),
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: path.resolve(__dirname, 'src/templates/index.html'),
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
+module.exports = function (config) {
+  config.set({
+    entry: {
+      app: path.resolve(__dirname, 'src/scripts/index.js'),
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+    module: {
+      rules: [
         {
-          from: path.resolve(__dirname, 'src/public/'),
-          to: path.resolve(__dirname, 'dist/'),
+          test: /\.css$/,
+          use: [
+            {
+              loader: 'style-loader',
+            },
+            {
+              loader: 'css-loader',
+            },
+          ],
         },
       ],
-    }),
-    new Dotenv({
-      path: path.resolve(__dirname, '.env'),
-      systemvars: true,
-      safe: true,
-    }),
-  ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: path.resolve(__dirname, 'src/templates/index.html'),
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'src/public/'),
+            to: path.resolve(__dirname, 'dist/'),
+          },
+        ],
+      }),
+      new Dotenv({
+        path: path.resolve(__dirname, '.env'),
+        systemvars: true,
+        safe: true,
+      }),
+    ],
+    webpack: {
+      devtool: 'inline-source-map',
+      mode: 'development',
+      plugins: [
+        new Dotenv({
+          path: path.resolve(__dirname, '.env'),
+          systemvars: true,
+          safe: true,
+        }),
+      ],
+    },
+    webpackMiddleware: {
+      stats: 'errors-only',
+    },
+  });
 };
